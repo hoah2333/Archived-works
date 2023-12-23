4a6f982703f8738c4ee656889343a522f062c571
hoah2333 <z13725385251@163.com>
2023-12-24 00:06:27 +0800

    need http?

--- SCP/scp-redirecter/main.js
+++ SCP/scp-redirecter/main.js
@@ -1,7 +1,7 @@
 $(document).ready(function () {
     var href = window.location.href;
     var hrefRegExp = /\?_?[A-Za-z0-9\-=]+/g;
-    var link = "https://scp-wiki-cn.wikidot.com/";
+    var link = "http://scp-wiki-cn.wikidot.com/";
 
     if (hrefRegExp.test(href)) {
         var arguments = href.slice(href.search(hrefRegExp) + 1);
@@ -11,7 +11,7 @@ $(document).ready(function () {
         window.location.replace(link);
     } else {
         alert("输入有误！正在将您重定向至 SCP 中文分部首页……");
-        window.location.replace('https://scp-wiki-cn.wikidot.com/');
+        window.location.replace('http://scp-wiki-cn.wikidot.com/');
     }
 
     function replace(arguments) {
@@ -32,9 +32,9 @@ $(document).ready(function () {
             if (site == undefined) {
                 site = "scp-wiki-cn";
             }
-            link = "https://" + site + ".wikidot.com/" + name;
+            link = "http://" + site + ".wikidot.com/" + name;
         } else {
-            link = "https://scp-wiki-cn.wikidot.com/" + arguments;
+            link = "http://scp-wiki-cn.wikidot.com/" + arguments;
         }
         return link;
     }
