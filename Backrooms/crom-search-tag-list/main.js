1414e3b292590b9bea71255979e09df8cb6d6cc7
hoah2333 <z13725385251@163.com>
2024-01-07 10:43:29 +0000

    Deploying to gh-pages from @ hoah2333/Archived-works@0d95af48e97152f7003bf79f6741b2763026d1be 🚀

---
+++ Backrooms/crom-search-tag-list/main.js
@@ -0,0 +1,52 @@
+$(document).ready(function () {
+    $("#main-input").load("http://backrooms-oversight-cn.wdfiles.com/local--code/crom-search-tag-list/1 pre", function (responseTxt) {
+        $("#main-input").val(responseTxt.slice(0, -1));
+    });
+    var wrapper_number = 1;
+
+    $("#add").click(function () {
+        wrapper_number = wrapper_number + 1;
+        append_content();
+    });
+    $("#minus").click(function () {
+        $(".wrapper" + wrapper_number).remove();
+        wrapper_number = wrapper_number - 1;
+        if (wrapper_number <= 1) {
+            wrapper_number = 1;
+        }
+    });
+
+    function append_content() {
+        var append_content = "";
+        append_content += "<div class=\"input-wrapper wrapper" + wrapper_number + "\">";
+        append_content += "<textarea class=\"input textarea1\" rows=\"1\"></textarea>";
+        append_content += "<p class=\"arrow\">-></p>";
+        append_content += "<textarea class=\"input textarea2\" rows=\"1\"></textarea>";
+        append_content += "</div>";
+        $("#addition-wrapper").append(append_content);
+    }
+
+    $("#confirm").click(function () {
+        var description = "这是一个 [https://toml.io/cn/v1.0.0 TOML] 配置文件，被 [https://backrooms-wiki-cn.wikidot.com/tag-search Crom 标签搜索]利用以为终端用户提供标签的纯文本描述。\n";
+        description += "\n";
+        description += "配置文件生成器在[http://backrooms-oversight-cn.wdfiles.com/local--code/crom-search-tag-list-manifester/1 此处]。\n";
+        description += "[[code type=\"toml\"]]\n";
+        var total_input = description;
+        total_input += $("#main-input").val();
+        for (output_number = 1; output_number <= wrapper_number; output_number++) {
+            if (total_input != description) {
+                total_input += "\n\n";
+            }
+            total_input += "[[tags]]\n";
+            total_input += "name = \"" + $(".wrapper" + output_number + " .textarea1").val() + "\"\n";
+            total_input += "description = \"" + $(".wrapper" + output_number + " .textarea2").val() + "\"";
+        }
+        total_input += "\n[[/code]]";
+        $("#output").val(total_input);
+    })
+
+    function check_input() {
+        var total_input = $("#main-input").val();
+        /* to do */
+    }
+});
---
+++ SCP/scp-redirecter/main.js
@@ -0,0 +1,47 @@
+$(document).ready(function () {
+    var href = window.location.href;
+    var hrefRegExp = /\?_?[A-Za-z0-9\-=/:]+/g;
+    var link = "https://scp-wiki-cn.wikidot.com/";
+
+    if (hrefRegExp.test(href)) {
+        var arguments = href.slice(href.search(hrefRegExp) + 1);
+
+        link = replace(arguments)
+
+        window.location.replace(link);
+    } else {
+        alert("输入有误！正在将您重定向至 SCP 中文分部首页……");
+        window.location.replace('https://scp-wiki-cn.wikidot.com/');
+    }
+
+    function replace(arguments) {
+        if (/=/g.test(arguments)) {
+            var args = arguments.split("&");
+            var name, site, url;
+            for (i = 0; i < args.length; i++) {
+                if (/name=[A-Za-z0-9\-/:]+/g.test(args[i])) {
+                    name = args[i].slice(args[i].search(/=[A-Za-z0-9\-/:]+/g) + 1);
+                }
+                if (/site=[A-Za-z0-9\-]+/g.test(args[i])) {
+                    site = args[i].slice(args[i].search(/=[A-Za-z0-9\-]+/g) + 1);
+                }
+                if (/url=[A-Za-z0-9\-/:]+/g.test(args[i])) {
+                    url = args[i].slice(args[i].search(/=[A-Za-z0-9\-/:]+/g) + 1);
+                }
+            }
+            if (name == undefined) {
+                name = "";
+            }
+            if (site == undefined) {
+                site = "scp-wiki-cn";
+            }
+            link = "https://" + site + ".wikidot.com/" + name;
+            if (url !== undefined) {
+                link = url;
+            }
+        } else {
+            link = "https://scp-wiki-cn.wikidot.com/" + arguments;
+        }
+        return link;
+    }
+})
